7e4494d16fe01481ea507e4d8e1250a85fe3f70f
zikra <zikraparween70@gmail,com>
2023-06-08 22:02:18 +0530

    this is 1st commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
 
+
+        class Product {
+            Product p = new Product();
+
+            // Task 3
+            public int product(int x, int y) {
+                return x * y;
+            }
+
+            // Task 4
+            public int product(int x, int y, int z) {
+                return x * y * z;
+            }
+
+            // Task 5
+            public double product(double x, double y) {
+                return x * y;
+            }
+        }
+    }
 }
